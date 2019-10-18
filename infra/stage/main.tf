9de6abd4c5942b7434b9b4cd3d8f5ed477b98375
Ma Chi <hellomarch@gmail.com>
2019-10-18 18:05:35 +0000

    use var.region

--- infra/stage/main.tf
+++ infra/stage/main.tf
@@ -3,21 +3,21 @@ variable region {
 }
 
 variable projectname {
-	default = "kilwapicture"
+	default = "kilwaphoto2"
 }
 
 module "storage" {
 	source = "../modules/storage"
-	region = "us-west-2"
+	region = var.region
 
-	photobucketname	=	"photo${var.projectname}"
-	codebucketname	=	"code${var.projectname}"
-	logbucketname	=	"log${var.projectname}"
+	photobucketname	= "photo${var.projectname}"
+	codebucketname	= "code${var.projectname}"
+	logbucketname	= "log${var.projectname}"
 }
 
 module "users" {
 	source = "../modules/users"
-	region = "us-west-2"
+	region = var.region
 
 	user_pool_name = "kilwafans"
 }
@@ -26,14 +26,14 @@ module "users" {
 data "template_file" "appjs" {
 	template = "${file("${path.module}/../../frontend/app_template.js")}"
 	vars = {
-		BUCKET_NAME	=  "photo${var.projectname}",
-		AWS_REGION	=	var.region	
+		BUCKET_NAME		=	"photo${var.projectname}",
+		AWS_REGION		=	var.region	
 		IDENTITY_POOL_ID	=	module.users.identity_pool_id
 	}
 }
 
 resource "local_file" "appjs" {
-    content     = data.template_file.appjs.rendered
+    content  = data.template_file.appjs.rendered
     filename = "${path.module}/../../frontend/app.js"
 }
 
